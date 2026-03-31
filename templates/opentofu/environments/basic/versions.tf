f6b615f96a342e0af16203613315d680d280e85e
sanguklee <sanguklee@sangukleeui-Macmini.local>
2026-03-31 21:50:05 +0900

    Fix: use hashicorp/openstack provider source

--- templates/opentofu/environments/basic/versions.tf
+++ templates/opentofu/environments/basic/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     openstack = {
-      source  = "terraform-provider-openstack/openstack"
+      source  = "hashicorp/openstack"
       version = ">= 1.54.0"
     }
   }
